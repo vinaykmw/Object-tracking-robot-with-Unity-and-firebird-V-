1e434b922c38660f74286086f63368c53a0087bd
vinay kushwaha <vinaykmw@gmail.com>
2018-08-18 20:35:45 +0530

    we are using vuforia plugin to track the object by using image marker
    Its easy to make , we simplified our project using vuforia plugin.
    Unity takes control of bot by serial communication.
    enable .NET 2.0, and vuforia in project settings .

---
+++ Object tracking bot Unity project/Assets/ardunio.cs
@@ -0,0 +1,93 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System.Collections;
+using System.IO;
+using System.IO.Ports;
+using System;
+using System.Threading;
+
+
+public class ardunio : MonoBehaviour {
+	public GameObject vrboxreferance;
+	private string rdata;
+	private string[] vec3;
+	private Vector3 accelnew ;
+	private Vector3 accel ;
+	private Vector3 dir;
+    public float veL, veR;
+
+	bool object_visiblity;
+	public GameObject targetRef;
+	public float dx; 
+	//public Rigidbody rb;
+
+	SerialPort sp = new SerialPort("COM3", 9600); //Set the port (com4) and the baud rate (9600, is standard on most devices) 
+
+	void Start () {
+        veL = 0;
+        veR = 0;
+		sp.Open ();
+	//	dx = Mathf.RoundToInt(targetRef.GetComponent<DistanceFinder> ().thetax);
+
+	}
+
+
+	void Update ()
+	{	dx = targetRef.GetComponent<DistanceFinder> ().thetax;
+		object_visiblity = vrboxreferance.GetComponent<MeshRenderer> ().isVisible;
+		portWriter ();
+	}
+
+
+
+	void portWriter()
+	{
+		//print (" running");
+		try
+		{	
+			// Check that the port is open. If not skip and do nothing
+			if (sp.IsOpen && object_visiblity&&(Mathf.Abs(dx)>0.5f))
+			{ float x = dx;
+                veL = 217.6414f + 2.007815f * x - 0.3077718f * Mathf.Pow(x, 2) + 0.001890171f * Mathf.Pow(x, 3) + 0.0002709233f * Mathf.Pow(x, 4) + 0.000001641281f * Mathf.Pow(x ,5);
+                veL = Mathf.Clamp(veL, 0, 250);
+                veR = 207.4261f - 0.9849778f * x - 0.1270757f * Mathf.Pow(x, 2) - 0.006143416f * Mathf.Pow(x, 3);
+                veR = Mathf.Clamp(veR, 0, 250);
+                GiveVelocity(Mathf.RoundToInt(veL), Mathf.RoundToInt(veR));
+            }
+			else{
+				Stop();
+				}
+		}
+		catch (Exception ex)
+		{
+			// This could be thrown if we close the port whilst the thread 
+			// is reading data. So check if this is the case!
+			if (sp.IsOpen) {
+				// Something has gone wrong!
+				Debug.Log (ex.Message.ToString ());
+				Stop ();
+			} else
+				print ("Port closed");
+		}
+
+	}
+	public void Stop()
+	{
+		print("stoping");
+
+
+		veL=0;
+		veR=0;
+		GiveVelocity(Mathf.RoundToInt(veL), Mathf.RoundToInt(veR));
+	}
+    //void OnTriggerEnter(Collider other)
+    public void GiveVelocity(int vl, int vr)
+	{//	print ("Giving velocity");
+		if (sp.IsOpen) {
+			sp.Write (vl.ToString () + 'l');
+			sp.Write (vr.ToString () + 'r');
+		}
+    }
+}
+
+
---
+++ Object tracking bot Unity project/Assets/ardunioManual.cs
@@ -0,0 +1,83 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System.Collections;
+using System.IO;
+using System.IO.Ports;
+using System;
+using System.Threading;
+
+
+public class ardunioManual  : MonoBehaviour {
+	public GameObject vrboxreferance;
+	private string rdata;
+	private string[] vec3;
+	private Vector3 accelnew ;
+	private Vector3 accel ;
+	private Vector3 dir;
+	public float veL, veR;
+
+	bool object_visiblity;
+	public GameObject targetRef;
+	public float dx; 
+	//public Rigidbody rb;
+
+	SerialPort sp = new SerialPort("COM3", 9600); //Set the port (com4) and the baud rate (9600, is standard on most devices) 
+
+	void Start () {
+		veL = 0;
+		veR = 0;
+		sp.Open ();
+		//	dx = Mathf.RoundToInt(targetRef.GetComponent<DistanceFinder> ().thetax);
+
+	}
+
+
+	void Update ()
+	{	dx = targetRef.GetComponent<DistanceFinder> ().thetax;
+		object_visiblity = vrboxreferance.GetComponent<MeshRenderer> ().isVisible;
+		portWriter ();
+		/*if (sp.IsOpen)
+			GiveVelocity(Mathf.RoundToInt(veL), Mathf.RoundToInt(veR));
+		else
+			sp.Open();
+			*/
+	}
+
+
+
+	void portWriter()
+	{
+		//print (" running");
+		try
+		{	
+			// Check that the port is open. If not skip and do nothing
+			if (sp.IsOpen )
+			{ 
+				GiveVelocity(Mathf.RoundToInt(veL), Mathf.RoundToInt(veR));
+				print("giving velocity ");
+			}
+			else{
+				sp.Open();
+				GiveVelocity(0,0);			}
+		}
+		catch (Exception ex)
+		{
+			// This could be thrown if we close the port whilst the thread 
+			// is reading data. So check if this is the case!
+			if (sp.IsOpen) {
+				// Something has gone wrong!
+				Debug.Log (ex.Message.ToString ());
+			} else
+				print ("Port closed");
+		}
+
+	}
+	//void OnTriggerEnter(Collider other)
+	public void GiveVelocity(int vl, int vr)
+	{
+		sp.Write(vl.ToString() + 'l');
+		sp.Write(vr.ToString() + 'r');
+	}
+}
+
+
